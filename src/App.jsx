8be975130544b81aa26cf3c8354430e7e0e24f03
steph <ntuigoodness3@gmail.com>
2024-08-12 14:35:40 +0100

    new collection

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,7 @@ function App() {
 
   return (
     <>
-     <h1>Welcome to Group B</h1>
-     <h2>Collaborators List , We are 6 in number</h2>
-     <ul>
-     <li>Matic</li>
-     <li>Dayo</li>
-     <li>Maureen</li>
-     <li>Chioma</li>
-    <li>Maureen</li>
-     <li>Steph</li>
-     </ul>
+     
     </>
   )
 }
