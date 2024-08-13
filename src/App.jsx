b01b93a62769ad1779755f637dfeea0a325a506a
Matic.dev <olaitantoyeeb1@gmail.com>
2024-08-13 12:51:14 +0100

    done with the hero and header and reponsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import Hero from "./Components/Hero";
 
 function App() {
-
   return (
     <>
-     
+      <Hero />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
