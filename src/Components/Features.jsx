f936300af25030991af17a4aa4504d14bea9626a
Chioma <okekelinda9991@gmail.com>
2024-08-13 23:53:42 -0700

    resolving conflicts

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -6,8 +6,8 @@ const Features = () => {
     <div className="features-flex">
       <div className="graphic-design">
         <div className="graphic-design-text">
-          <h4>Graphic Design</h4>
-          <p>
+          <h4 className="fw-900 text-cyan">Graphic Design</h4>
+          <p className="fw-900 text-cyan">
             Great design makes you memorable. We deliver artwork that
             underscores your brand message and captures potential client's
             attention.
@@ -17,8 +17,8 @@ const Features = () => {
       
       <div className="photography">
         <div className="photography-text">
-          <h4>Photography</h4>
-          <p>
+          <h4 className="fw-900 text-dark-blue">Photography</h4>
+          <p className="fw-900 text-dark-blue">
             Increase your credibility by getting the most stunning, high-quality
             photos that improve your business image.
           </p>
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -7,11 +7,11 @@ const Services = () => {
   return (
     <div className='section-grid'>
             <section className='section-one services'>
-                <h2>Transform your brand</h2>
-                <p>We are a full-service creative agency specializing in helping brands grow fast.
+                <h2 className='text-desaturated-blue fw-900'>Transform your brand</h2>
+                <p className='fs-900 fw-600'>We are a full-service creative agency specializing in helping brands grow fast.
                     Engage your clients through compelling visuals that do most of the marketing for you.
                 </p>
-                <a href="" className='uppercase'>Learn more</a>
+                <a href="" className='uppercase fw-900'>Learn more</a>
             </section>
 
             <section className='section-two services'>
@@ -22,12 +22,12 @@ const Services = () => {
                <img src={standoutImage} alt="a pink cup" />
             </section>
 
-            <section className='section-four services'>
-                <h2>Stand out to the right audience</h2>
-                <p>Using a collaborative formula of designers, researchers, photographers,
+            <section className='section-four services fs-900'>
+                <h2 className='text-desaturated-blue fw-900'>Stand out to the right audience</h2>
+                <p className='fs-900 fw-600'>Using a collaborative formula of designers, researchers, photographers,
                   videographers, and copywriters, we'll build and extend your brandin digital places.
                 </p>
-                <a href="" className='uppercase'>Learn more</a>
+                <a href="" className='uppercase fw-900'>Learn more</a>
             </section>
     </div>
   )
